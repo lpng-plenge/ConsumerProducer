60a6c08dece6121c7f79fd40eef56d51d47032bf
lpng-plenge <luispablonieto@gmail.com>
2022-03-09 16:57:35 -0600

    PreDebugging

---
+++ ConsumerProducer/Monitor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsumerProducer
+{
+    class Monitor
+    {
+        private static int bufferSize = 0;
+        private static int counter = 0;
+        private static bool noInterested= false;
+
+        private static List<double> list = new List<double>();
+
+        public static void append(double x)=>agregarAzync(x);
+        public static void take() => consumirAsync();
+
+        private static async Task agregarAzync(double x)
+        {
+            bufferSize = list.Count;
+            while (counter.Equals(bufferSize) && noInterested ==true) await Task.Delay(1000);
+            noInterested = true;
+            list.Add(x);
+            counter++;
+            noInterested = false;
+        }
+
+        private static async Task consumirAsync()
+        {
+            while (counter.Equals(0)||counter<0 && noInterested==true) await Task.Delay(1000);
+            noInterested = true;
+            double x = consulta();
+            list.Remove(x);
+            counter--;
+            noInterested = false;
+        }
+
+        private static int consulta() { Random ran = new Random(); return ran.Next(list.Count); }
+
+    }
+}
--- ConsumerProducer/Program.cs
+++ ConsumerProducer/Program.cs
@@ -1,13 +1,37 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsumerProducer
 {
     class Program
     {
+        Random random = new Random();
+        //MUTEX
+        private static Mutex mut = new Mutex();
+        private static int numThreads = 2; //cambiar con la interfaz
+
+        //List Prueba 1
         static void Main(string[] args)
         {
-            
+            //call the threads producer and consumer 
+               
+        }
 
+        private void producer()
+        {
+            while (true)
+            {
+                double x = random.Next(1000);
+                Monitor.append(x);        
+            }
+        }
+
+        private void consumer()
+        {
+            while (true)
+            {
+                Monitor.take();
+            }
         }
     }
 }
